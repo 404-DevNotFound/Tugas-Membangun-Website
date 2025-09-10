f6185d8009936c7bb508788c1370afd9f8b6427b
MTHermawan <trigusnihermawan@gmail.com>
2025-09-11 00:04:01 +0800

    Add Javascript form confirmation

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const password = document.getElementById("password");
 const username = document.getElementById("username");
 
 button.addEventListener("click", function (e) {
-  if (password.value.length > 16) {
-    e.preventDefault();
-    alert("Password tidak boleh lebih dari 16 karakter");
-    password.focus();
-  } else if (username.value.length > 24) {
+  
+  if (username.value.length > 24) {
     e.preventDefault();
     alert("Username tidak boleh lebih dari 24 karakter");
     username.focus();
+  } else if (password.value.length > 16) {
+    e.preventDefault();
+    alert("Password tidak boleh lebih dari 16 karakter");
+    password.focus();
   } else {
     window.location.href = "home.html";
   }
